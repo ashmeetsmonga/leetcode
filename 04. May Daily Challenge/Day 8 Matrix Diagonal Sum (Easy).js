2a00b72a40080b5ee9af065d1247776de42d97c8
ashmeetsmonga <ashmeetsmonga@gmail.com>
2023-05-08 23:46:43 +0530

    day 8 done

---
+++ 04. May Daily Challenge/Day 8 Matrix Diagonal Sum (Easy).js
@@ -0,0 +1,45 @@
+// 1572. Matrix Diagonal Sum
+
+// Given a square matrix mat, return the sum of the matrix diagonals.
+
+// Only include the sum of all the elements on the primary diagonal and all the elements on the secondary diagonal that are not part of the primary diagonal.
+
+// Example 1:
+
+// Input: mat = [[1,2,3],
+//               [4,5,6],
+//               [7,8,9]]
+// Output: 25
+// Explanation: Diagonals sum: 1 + 5 + 9 + 3 + 7 = 25
+// Notice that element mat[1][1] = 5 is counted only once.
+// Example 2:
+
+// Input: mat = [[1,1,1,1],
+//               [1,1,1,1],
+//               [1,1,1,1],
+//               [1,1,1,1]]
+// Output: 8
+// Example 3:
+
+// Input: mat = [[5]]
+// Output: 5
+
+// Constraints:
+
+// n == mat.length == mat[i].length
+// 1 <= n <= 100
+// 1 <= mat[i][j] <= 100
+
+// Code
+var diagonalSum = function (mat) {
+	let m = mat.length;
+	let n = mat[0].length;
+
+	let ans = 0;
+	for (let i = 0; i < m; i++) {
+		ans = ans + mat[i][i] + mat[i][n - i - 1];
+		if (i === n - i - 1) ans = ans - mat[i][i];
+	}
+
+	return ans;
+};
